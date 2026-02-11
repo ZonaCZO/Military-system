ed627df37bf90f1dc8b8ea90de5068fd9e889b74
Nikita <nikitaprox93@gmail.com>
2026-02-11 20:10:31 +0100

    Installer

--- install/install.lua
+++ install/install.lua
@@ -18,10 +18,25 @@ if choice == "1" then
     download("data/deaddrop.lua", "deaddrop.lua")
     print("Command system installed.")
 elseif choice == "2" then
-    download("server/resistance_core.lua", "server")
+    download("server/resistance_core.lua", "server.lua")
     print("Server system installed.")
 else
     print("Invalid choice.")
 end
 
 print("Installation complete.")
+
+print("Configuring auto-boot...")
+local f = fs.open("startup.lua", "w")
+if choice == "1" then
+    f.write('shell.run("server.lua")')
+elseif choice == "2" then
+    f.write('shell.run("fg commander.lua")')
+end
+f.close()
+
+term.setTextColor(colors.yellow)
+print("\nINSTALLED.")
+print("Press Enter to Reboot and Host.")
+read()
+os.reboot()
